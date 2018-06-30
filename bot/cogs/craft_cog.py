566278e625eb2460870116499b543de974abc1b9
Caidan Williams <caidanstevenwilliams@gmail.com>
2018-06-30 12:52:41 -0600

    added new 'recipe' command to list an items recipe
    - recipe details the items needed to craft a recipe
    - fixed small error with trying to find recipe name in the Recipe enum
    - added doc string to craft command
    - added method to enum to format the recipe dict to a string

--- bot/cogs/craft_cog.py
+++ bot/cogs/craft_cog.py
@@ -14,13 +14,14 @@ class CraftCog:
 
     @commands.command(pass_context=True)
     async def craft(self, context, *item_to_craft):
+        """ Craft a new item. """
         author = context.message.author
         inventory = Game.get_player(author).inventory
 
         recipe_name = '_'.join(item_to_craft)
         display_name = ' '.join(item_to_craft)
 
-        if recipe_name not in Recipe:
+        if not Recipe[recipe_name]:
             return await self.bot.say(f'Could not find the recipe for "{recipe_name}".')
 
         recipe = Recipe[recipe_name].needs_items()
@@ -37,6 +38,35 @@ class CraftCog:
 
         inventory.add_craftable(CraftableItem[recipe_name])
         await self.bot.edit_message(message, f'{author.mention} finished crafting {display_name}')
+
+    @commands.group(pass_context=True)
+    async def recipe(self, context, *item_name):
+        """ Get the recipe of the desired craftable item. """
+        if context.invoked_subcommand:
+            # ignore everything in this function if there was a subcommand passed
+            return
+
+        if not item_name:
+            return await self.bot.say('A recipe name is required.')
+
+        recipe_name = '_'.join(item_name)
+        display_name = ' '.join(item_name)
+
+        if not Recipe[recipe_name]:
+            return await self.bot.say(f'Could not find the recipe for "{display_name}".')
+
+        recipe = Recipe[recipe_name]
+
+        return await self.bot.say(recipe.to_string())
+
+    @recipe.command()
+    async def all(self):
+        """ Get all of the recipes of the craftable items. """
+        output = '```\n'
+        for item in Recipe:
+            name = item.name.replace('_', ' ')
+            name = name.title()
+            print(name)
 
 
 def setup(bot):
--- game/enums/recipe.py
+++ game/enums/recipe.py
@@ -13,3 +13,15 @@ class Recipe(Enum):
 
     def needs_items(self):
         return self.value
+
+    def to_string(self):
+        output = '```\n'
+        header = self.name.title().replace('_', ' ') + ' Recipe'
+        output += header
+        output += '\n' + '-' * len(header)
+        for item, amt in self.value.items():
+            name = item.name.ljust(10)
+            amt = str(amt).zfill(3)
+            output += f'\n{name} : {amt}'
+        output += '\n```'
+        return output
