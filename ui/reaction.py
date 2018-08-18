292a2e86fac38ebbed43f13475d90b09a2ca24c7
Caidan Williams <caidanstevenwilliams@gmail.com>
2018-08-18 15:29:38 -0600

    added new reactions

--- ui/reaction.py
+++ ui/reaction.py
@@ -2,6 +2,11 @@ from enum import Enum
 
 
 class Reaction(Enum):
-    DISMISS = '\u274E'      # ❎
-    CONFIRM = '\u2705'      # ✅
-    MAILBOX = "\U0001f4eb"  # 📫
+    DISMISS = '\u274E'          # ❎
+    CONFIRM = '\u2705'          # ✅
+    MAILBOX = '\U0001f4eb'      # 📫
+
+    CROSSED_SWORDS = '\u2694'   # ⚔️
+    SHIELD = '\U0001f6e1'       # 🛡️
+    PACKAGE = '\U0001f4e6'      # 📦
+    WHITE_FLAG = '\U0001f3f3'   # 🏳️
