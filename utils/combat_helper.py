0962c8e960f3988df893e025e77f7310b927e8f0
Caidan Williams <caidanstevenwilliams@gmail.com>
2018-08-20 13:23:46 -0600

    filled out the rest of the description messages for player actions

--- utils/combat_helper.py
+++ utils/combat_helper.py
@@ -27,18 +27,18 @@ combat_descriptions = {
         Reaction.WHITE_FLAG: 'You defended, and {opponent} surrendered.'
     },
     Reaction.PACKAGE: {
-        Reaction.NO_ENTRY_SIGN: 'TBD',
-        Reaction.CROSSED_SWORDS: 'TBD',
-        Reaction.SHIELD: 'TBD',
-        Reaction.PACKAGE: 'TBD',
-        Reaction.WHITE_FLAG: 'TBD'
+        Reaction.NO_ENTRY_SIGN: 'You use an item, while {opponent} stares at you.',
+        Reaction.CROSSED_SWORDS: 'You use an item, while {opponent} attacks. You took {received_dmg} damage.',
+        Reaction.SHIELD: 'You use an item, and {opponent} defends. Nothing happens.',
+        Reaction.PACKAGE: 'You use an item, and {opponent} uses an item.',
+        Reaction.WHITE_FLAG: 'You use an item, while {opponent} surrenders.'
     },
     Reaction.WHITE_FLAG: {
-        Reaction.NO_ENTRY_SIGN: 'TBD',
-        Reaction.CROSSED_SWORDS: 'TBD',
-        Reaction.SHIELD: 'TBD',
-        Reaction.PACKAGE: 'TBD',
-        Reaction.WHITE_FLAG: 'TBD'
+        Reaction.NO_ENTRY_SIGN: 'You surrender, while {opponent} stares at you.',
+        Reaction.CROSSED_SWORDS: 'You surrender, and {opponent} attacks. You take {received_dmg} damage.',
+        Reaction.SHIELD: 'You surrender, and {opponent} defends.',
+        Reaction.PACKAGE: 'You surrender, while {opponent} uses an item.',
+        Reaction.WHITE_FLAG: 'You surrender, and {opponent} surrenders.'
     }
 }
 
@@ -58,25 +58,24 @@ async def handle_combat_actions(player_1, p1_action, player_2, p2_action):
     p1_desc = combat_descriptions.get(p1_action)
     p2_desc = combat_descriptions.get(p2_action)
 
-    p1_msg = p1_desc.get(p2_action, 'No support for current action')
-    p2_msg = p2_desc.get(p1_action, 'No support for current action')
+    p1_desc_values = {
+        'opponent': player_2.username
+    }
+    p2_desc_values = {
+        'opponent': player_1.username
+    }
 
     if p1_action == Reaction.NO_ENTRY_SIGN:
         if p2_action == Reaction.NO_ENTRY_SIGN:
-            p1_msg = p1_msg.format(opponent=player_2.username)
-            p2_msg = p2_msg.format(opponent=player_1.username)
+            pass  # don't need to do anything here, as we already have the opponent value
         elif p2_action == Reaction.CROSSED_SWORDS:
             actual_damage = get_critical_damage(player_2.damage)
             player_1.health -= actual_damage
 
-            p1_msg = p1_msg.format(opponent=player_2.username, received_dmg=actual_damage)
-            p2_msg = p2_msg.format(opponent=player_1.username, dealt_dmg=actual_damage)
+            p1_desc_values['received_dmg'] = actual_damage
+            p2_desc_values['dealt_dmg'] = actual_damage
         elif p2_action == Reaction.SHIELD:
-            actual_damage = get_damage_after_defense(player_1.damage, player_2.defense)
-            player_2.health -= actual_damage
-
-            p1_msg = p1_msg.format(opponent=player_2.username)
-            p2_msg = p2_msg.format(opponent=player_1.username)
+            pass
         elif p2_action == Reaction.PACKAGE:
             pass
         elif p2_action == Reaction.WHITE_FLAG:
@@ -86,37 +85,45 @@ async def handle_combat_actions(player_1, p1_action, player_2, p2_action):
             actual_damage = get_critical_damage(player_1.damage)
             player_2.health -= actual_damage
 
-            p1_msg = p1_msg.format(opponent=player_2.username, dealt_dmg=actual_damage)
-            p2_msg = p2_msg.format(opponent=player_1.username, received_dmg=actual_damage)
+            p1_desc_values['dealt_dmg'] = actual_damage
+            p2_desc_values['received_dmg'] = actual_damage
         elif p2_action == Reaction.CROSSED_SWORDS:
             player_1.health -= player_2.damage
             player_2.health -= player_1.damage
 
-            p1_msg = p1_msg.format(opponent=player_2.username, dealt_dmg=player_1.damage, received_dmg=player_2.damage)
-            p2_msg = p2_msg.format(opponent=player_1.username, dealt_dmg=player_2.damage, received_dmg=player_1.damage)
+            p1_desc_values['dealt_dmg'] = player_1.damage
+            p1_desc_values['received_dmg'] = player_2.damage
+            p2_desc_values['dealt_dmg'] = player_2.damage
+            p2_desc_values['received_dmg'] = player_1.damage
         elif p2_action == Reaction.SHIELD:
             actual_damage = get_damage_after_defense(player_1.damage, player_2.defense)
             player_2.health -= actual_damage
 
-            p1_msg = p1_msg.format(opponent=player_2.username, dealt_dmg=actual_damage)
-            p2_msg = p2_msg.format(opponent=player_1.username, blocked_dmg=player_2.defense, received_dmg=actual_damage)
+            p1_desc_values['dealt_dmg'] = actual_damage
+            p2_desc_values['blocked_dmg'] = player_2.defense
+            p2_desc_values['received_dmg'] = actual_damage
         elif p2_action == Reaction.PACKAGE:
-            pass
+            player_2.health -= player_1.damage
+
+            p1_desc_values['dealt_dmg'] = player_1.damage
+            p2_desc_values['received_dmg'] = player_1.damage
         elif p2_action == Reaction.WHITE_FLAG:
-            pass
+            player_2.health -= player_1.damage
+
+            p1_desc_values['dealt_dmg'] = player_1.damage
+            p2_desc_values['received_dmg'] = player_1.damage
     elif p1_action == Reaction.SHIELD:
         if p2_action == Reaction.NO_ENTRY_SIGN:
-            p1_msg = p1_msg.format(opponent=player_2.username)
-            p2_msg = p2_msg.format(opponent=player_1.username)
+            pass
         elif p2_action == Reaction.CROSSED_SWORDS:
             actual_damage = get_damage_after_defense(player_2.damage, player_1.defense)
             player_1.health -= actual_damage
 
-            p1_msg = p1_msg.format(opponent=player_2.username, blocked_dmg=player_1.defense, received_dmg=actual_damage)
-            p2_msg = p2_msg.format(opponent=player_1.username, dealt_dmg=actual_damage)
+            p1_desc_values['blocked_dmg'] = player_1.defense
+            p1_desc_values['received_dmg'] = actual_damage
+            p2_desc_values['dealt_dmg'] = actual_damage
         elif p2_action == Reaction.SHIELD:
-            p1_msg = p1_msg.format(opponent=player_2.username)
-            p2_msg = p2_msg.format(opponent=player_1.username)
+            pass
         elif p2_action == Reaction.PACKAGE:
             pass
         elif p2_action == Reaction.WHITE_FLAG:
@@ -125,7 +132,10 @@ async def handle_combat_actions(player_1, p1_action, player_2, p2_action):
         if p2_action == Reaction.NO_ENTRY_SIGN:
             pass
         elif p2_action == Reaction.CROSSED_SWORDS:
-            pass
+            player_1.health -= player_2.damage
+
+            p1_desc_values['received_dmg'] = player_2.damage
+            p2_desc_values['dealt_dmg'] = player_1.damage
         elif p2_action == Reaction.SHIELD:
             pass
         elif p2_action == Reaction.PACKAGE:
@@ -136,13 +146,19 @@ async def handle_combat_actions(player_1, p1_action, player_2, p2_action):
         if p2_action == Reaction.NO_ENTRY_SIGN:
             pass
         elif p2_action == Reaction.CROSSED_SWORDS:
-            pass
+            player_1.health -= player_2.damage
+
+            p1_desc_values['received_dmg'] = player_2.damage
+            p2_desc_values['dealt_dmg'] = player_1.damage
         elif p2_action == Reaction.SHIELD:
             pass
         elif p2_action == Reaction.PACKAGE:
             pass
         elif p2_action == Reaction.WHITE_FLAG:
             pass
+
+    p1_msg = p1_desc.get(p2_action).format(p1_desc_values)
+    p2_msg = p2_desc.get(p1_action).format(p2_desc_values)
 
     p1_msg = f'```\n{p1_msg}\n```'
     p2_msg = f'```\n{p2_msg}\n```'
