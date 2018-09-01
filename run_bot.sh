d7acac22d2e4762e142660086f47e2b1af578bdd
Caidan Williams <caidanstevenwilliams@gmail.com>
2018-09-01 14:35:28 -0600

    duplicate commands and left over stuff

--- run_bot.sh
+++ run_bot.sh
@@ -1,8 +1,3 @@
-echo "Running Isle-Bot"
-sh -c 'echo $$ > .bot_pid; exec nohup python3.6 isle_bot.py >> logs.txt 2> errors.txt'
-
-----------------------------------------------------------------------------------------------
-
 echo "Starting Isle-Bot"
 nohup pipenv run python3.6 isle_bot.py >> logs.txt & BPID=$(echo $!)
 printf "Isle-Bot started successfully\n\tPID: $BPID\n"
