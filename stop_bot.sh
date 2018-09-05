8cfc53373a4238113263ff81e3973542605b9261
Caidan Williams <caidanstevenwilliams@gmail.com>
2018-09-04 23:48:40 -0600

    stop shell script doesn't work...

--- stop_bot.sh
+++ stop_bot.sh
@@ -1,12 +1,2 @@
 #!/usr/bin/env bash
-if [ "$1" == "-d" ]; then
-    echo "Logging out Isle-Bot | dev-build"
-    bash discord.sh --webhook-url `cat webhooks_discord/dev_webhook_url` --text "?create"
-    bash discord.sh --webhook-url `cat webhooks_discord/dev_webhook_url` --text "?logout"
-    echo "Logged out Isle-Bot | dev-build"
-else
-    echo "Logging out Isle-Bot"
-    bash discord.sh --webhook-url `cat webhooks_discord/webhook_url` --text "?create"
-    bash discord.sh --webhook-url `cat webhooks_discord/webhook_url` --text "?logout"
-    echo "Logged out Isle-Bot"
-fi
+echo "Log out of Isle-Bot with ?logout"
