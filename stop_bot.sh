cfb6aaa43864286592312fa7a05160b915f205a0
Caidan Williams <caidanstevenwilliams@gmail.com>
2018-09-04 20:44:21 -0600

    updated stop_bot.sh to make commands work

--- stop_bot.sh
+++ stop_bot.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 if [ "$1" == "-d" ]; then
     echo "Logging out Isle-Bot | dev-build"
+    bash discord.sh --webhook-url `cat webhooks_discord/dev_webhook_url` --text "?create"
     bash discord.sh --webhook-url `cat webhooks_discord/dev_webhook_url` --text "?logout"
     echo "Logged out Isle-Bot | dev-build"
 else
     echo "Logging out Isle-Bot"
+    bash discord.sh --webhook-url `cat webhooks_discord/webhook_url` --text "?create"
     bash discord.sh --webhook-url `cat webhooks_discord/webhook_url` --text "?logout"
     echo "Logged out Isle-Bot"
 fi
