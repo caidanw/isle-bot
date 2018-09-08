f1d1dfc7941125066ccbff5a15a77f58e8aa2d60
Ubuntu <ubuntu@ip-172-31-16-181.us-east-2.compute.internal>
2018-09-08 15:43:21 +0000

    updated run script to save bot pid to file

--- run_bot.sh
+++ run_bot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "Starting Isle-Bot"
-pipenv run python3.6 isle_bot.py &
-echo "Isle-Bot started successfully"
+pipenv run python3.6 isle_bot.py & BPID=$(echo $!)
+printf "Isle-Bot started successfully\n\tPID: $BPID\n"
+echo ${BPID} > .bot_pid
