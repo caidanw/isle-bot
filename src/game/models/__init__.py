91817033d8dd201590f439d4d09138adf248eec1
Caidan Williams <caidanstevenwilliams@gmail.com>
2019-01-26 16:01:20 -0700

    added __init__.py files to make folders packages

---
+++ src/game/models/__init__.py
@@ -0,0 +1,15 @@
+from .inventory import Inventory
+from .island import Island
+from .player import Player
+from .player_stat import PlayerStat
+from .resource import Resource
+from .union import Union
+
+all_models = [
+    Inventory,
+    Island,
+    Player,
+    PlayerStat,
+    Resource,
+    Union
+]
